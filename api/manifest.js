49015b7ce7236bd31c9cb94e702649d9e4f85287
infighter0034-beep <infighter0034@gmail.com>
2026-07-11 12:23:02 +0530

    Update manifest.js

--- api/manifest.js
+++ api/manifest.js
@@ -1,12 +1,37 @@
 module.exports = (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+
   res.status(200).json({
     id: "org.jaatog.addon",
     version: "1.0.0",
+
     name: "JaatOG Addon",
-    description: "Private Movies & Series",
-    resources: ["stream", "meta"],
-    types: ["movie", "series"],
+
+    description: "Personal Movies & Series",
+
+    logo: "https://jaatog-addon.vercel.app/logo.png",
+
+    background: "https://jaatog-addon.vercel.app/background.jpg",
+
+    types: [
+      "movie",
+      "series"
+    ],
+
+    resources: [
+      "meta",
+      "stream"
+    ],
+
     catalogs: [],
-    idPrefixes: ["tt"]
+
+    idPrefixes: [
+      "tt"
+    ],
+
+    behaviorHints: {
+      configurable: false,
+      configurationRequired: false
+    }
   });
 };
