61043482742364c80879adbe391a488b62dd53ea
infighter0034-beep <infighter0034@gmail.com>
2026-07-11 11:53:02 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "JaatOG Addon",
+  description: "Private Media Addon"
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
